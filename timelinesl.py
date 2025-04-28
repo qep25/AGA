7ab1a340728ee1c5b76310d9689455192c77ca11
qep25 <106804276+qep25@users.noreply.github.com>
2025-04-28 14:47:34 +0800

    Update timelinesl.py

--- timelinesl.py
+++ timelinesl.py
@@ -140,11 +140,11 @@ if view_mode == "Gantt Chart":
         )
 
         if show_border:
-            fig.update_traces(marker_line_color='black', marker_line_width=1)
+            fig.update_traces(marker_line_color='black', marker_line_width=0.5)
 
         if add_today_line:
             today = datetime.today()
-            fig.add_vline(x=today, line_width=2, line_dash="dash", line_color="red")
+            fig.add_vline(x=today, line_width=1, line_dash="dash", line_color="red")
 
         st.plotly_chart(fig, use_container_width=True)
     else:
